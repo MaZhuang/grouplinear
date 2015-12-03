5cde1306885004810a15193a895ea6d1f73162e8
Zhuang Ma <zhuangma@wharton.upenn.edu>
2015-12-03 00:37:25 -0500

    add dynamicMin

--- dynamic_sure.R
+++ dynamic_sure.R
@@ -15,15 +15,51 @@ DynamicSure=function(x,v){
 			sure=sure.spher(x[i:j], v[i:j])
 		#	print(sure)
 			a[i,j]=j
+			b[i,j]=sure
 			for (k in i:(j-1)){
-				temp=sure.spher(x[i:k], v[i:k])+sure.spher(x[(k+1):j], v[(k+1):j])
-				if (sure>temp){
-					sure=temp
+				#temp=sure.spher(x[i:k], v[i:k])+sure.spher(x[(k+1):j], v[(k+1):j])
+				temp=b[i,k]+b[k+1,j]
+				if (b[i,j]>temp){
 				#	print(sure)
 					a[i,j]=k
-					b[i,j]=sure
+					b[i,j]=temp
 				}
 			}
+			
+		}
+	}
+	list(a,b)
+}
+
+DynamicSureMin=function(x,v,d=40){
+    n <- length(x)
+    a=matrix(rep(0,n*n),ncol=n) ##separation
+	b=a ##value
+	for (i in 1:(n-d+1)){
+		j=i+d-1
+		a[i,j]=j
+		b[i,j]=sure.spher(x[i:j], v[i:j])
+	}
+	for (l in d:(n-1)){
+		if (l %% 20==0){
+		    print(l)
+		}
+		for (i in 1:(n-l)){
+			j=l+i
+			sure=sure.spher(x[i:j], v[i:j])
+		#	print(sure)
+			a[i,j]=j
+			b[i,j]=sure
+			if ((i+d-1)<=(j-d)){
+			for (k in (i+d-1):(j-d)){
+				#temp=sure.spher(x[i:k], v[i:k])+sure.spher(x[(k+1):j], v[(k+1):j])
+				temp=b[i,k]+b[k+1,j]
+				if (b[i,j]>temp){
+				#	print(sure)
+					a[i,j]=k
+					b[i,j]=temp
+				}
+			}}
 			
 		}
 	}
@@ -68,3 +104,13 @@ GroupSure<- function(x,v){
    return(est)
 }
 
+GroupSureMin<- function(x,v,d){ 
+   c=DynamicSureMin(x,v,d)
+   position=c[[1]]
+   n=dim(position)[1]
+   group=partition(position,1,n)
+   group=c(0, group,n)
+   group=unique(group)
+   est=dynamic.grouplinear(x,v,group)
+   return(est)
+}
--- shuffle.R
+++ shuffle.R
@@ -1,5 +1,5 @@
 bat.perm <- function(){
-  bat <- bat.raw[1:150,]
+  bat <- bat.raw
   bat$N1 <- bat$AB.4. + bat$AB.5. + bat$AB.6.  # total number at-bats for 1st period
   bat$N2 <- bat$AB.7. + bat$AB.8. + bat$AB.9.10.  # total number at-bats for 2nd period
   bat$H1 <- bat$H.4. + bat$H.5. + bat$H.6.  # total number hits for 1st period
@@ -40,6 +40,10 @@ tse.gl <- rep(NA,N)
 tse.gl.ol <- rep(NA,N)
 tse.gl.sure <- rep(NA,N)
 tse.gl.dynamic <- rep(NA,N)
+tse.gl.dynamicMin <- rep(NA,N)
+tse.gl.dynamicMin2 <- rep(NA,N)
+tse.gl.dynamicMin3 <- rep(NA,N)
+
 path=getwd()
 datapath=paste(path,'Brown_batting_data.txt',sep='/')
 bat.raw <- read.table(datapath, header=TRUE, sep=",", quote="")
@@ -48,7 +52,9 @@ for(j in 1:N){
   bat <- bat.perm()
   cat('Loop:',j)
   index=order(bat$N1,decreasing=TRUE)
+
   bat=bat[index,]
+  n=dim(bat)[1]
   # estimating TSE for various estimators
   # run: functions.R(current folder), functions_XKB.R
   ind <- bat$N2>10  # indicator for records with N2>=11 (among those with N1>=11)
@@ -103,11 +109,22 @@ for(j in 1:N){
   tse.gl.sure[j] <- tse.hat.delta.gl.sure/tse.hat.zero
   
   
-
 delta.dynamic=GroupSure(bat$X1,1/(4 * bat$N1))
 tse.hat.delta.dynamic <- sum(   (  ( bat$X2 - delta.dynamic )^2 - 1/ ( 4 * bat$N2 )  )[ind]   )
 tse.gl.dynamic[j] =tse.hat.delta.dynamic/tse.hat.zero
 
+
+delta.dynamicMin=GroupSureMin(bat$X1,1/(4 * bat$N1),40)
+tse.hat.delta.dynamicMin <- sum(   (  ( bat$X2 - delta.dynamicMin )^2 - 1/ ( 4 * bat$N2 )  )[ind]   )
+tse.gl.dynamicMin[j] =tse.hat.delta.dynamicMin/tse.hat.zero
+
+delta.dynamicMin2=GroupSureMin(bat$X1,1/(4 * bat$N1),50)
+tse.hat.delta.dynamicMin2 <- sum(   (  ( bat$X2 - delta.dynamicMin2 )^2 - 1/ ( 4 * bat$N2 )  )[ind]   )
+tse.gl.dynamicMin2[j] =tse.hat.delta.dynamicMin/tse.hat.zero
+
+delta.dynamicMin3=GroupSureMin(bat$X1,1/(4 * bat$N1),60)
+tse.hat.delta.dynamicMin3 <- sum(   (  ( bat$X2 - delta.dynamicMin3 )^2 - 1/ ( 4 * bat$N2 )  )[ind]   )
+tse.gl.dynamicMin3[j] =tse.hat.delta.dynamicMin/tse.hat.zero
 }
 
 tse.gm.all <- mean(tse.gm)
@@ -117,8 +134,13 @@ tse.gl.all <- mean(tse.gl)
 tse.gl.ol.all <- mean(tse.gl.ol)
 tse.gl.sure.all <- mean(tse.gl.sure)
 tse.gl.dynamic.all <- mean(tse.gl.dynamic)
-average=c(tse.gm.all,tse.M.all,tse.SG.all,tse.gl.all,tse.gl.ol.all,tse.gl.sure.all,tse.gl.dynamic.all )
-error=cbind(tse.gm,tse.M,tse.SG,tse.gl,tse.gl.ol,tse.gl.sure,tse.gl.dynamic)
+tse.gl.dynamicMin.all <- mean(tse.gl.dynamicMin)
+tse.gl.dynamicMin2.all <- mean(tse.gl.dynamicMin2)
+tse.gl.dynamicMin3.all <- mean(tse.gl.dynamicMin3)
+
+
+average=c(tse.gm.all,tse.M.all,tse.SG.all,tse.gl.all,tse.gl.ol.all,tse.gl.sure.all,tse.gl.dynamic.all,tse.gl.dynamicMin.all,tse.gl.dynamicMin2.all,tse.gl.dynamicMin3.all )
+error=cbind(tse.gm,tse.M,tse.SG,tse.gl,tse.gl.ol,tse.gl.sure,tse.gl.dynamic,tse.gl.dynamicMin,tse.gl.dynamicMin2,tse.gl.dynamicMin3)
 
 names=colnames(error)
 write.table(average, "~/desktop/average.txt",sep="\t",row.names=names)
